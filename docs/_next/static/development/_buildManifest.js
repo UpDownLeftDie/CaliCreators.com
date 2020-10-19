95f644f1effc9e9a27ab4f3ec16173dd8902a162
Jared K <jaredkotoff@users.noreply.github.com>
2020-10-19 14:04:05 -0700

    fix: clean up dev files

--- docs/_next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/[group]/extralife":["static\u002Fchunks\u002Fpages\u002F[group]\u002Fextralife.js"],sortedPages:["\u002F_app","\u002F_error","\u002F[group]\u002Fextralife"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
